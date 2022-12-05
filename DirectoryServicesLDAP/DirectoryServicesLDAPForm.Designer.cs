f52aece53cc3039ce89f654c29784f69e4db21c4
yasinsunmaz <yasinsunmaz13@gmail.com>
2022-12-05 21:25:13 +0300

    Active Directory DirectoryServices ile kimlik doğrulama ve kullanı arama

---
+++ DirectoryServicesLDAP/DirectoryServicesLDAPForm.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace DirectoryServicesLDAP
+{
+  partial class DirectoryServicesLDAPForm
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.groupBoxAdminInfo = new System.Windows.Forms.GroupBox();
+      this.comboBoxConnectionType = new System.Windows.Forms.ComboBox();
+      this.lbl5 = new System.Windows.Forms.Label();
+      this.chckDisableCertCheck = new System.Windows.Forms.CheckBox();
+      this.comboBoxPort = new System.Windows.Forms.ComboBox();
+      this.txtAdminUserName = new System.Windows.Forms.TextBox();
+      this.isAdminConnect = new System.Windows.Forms.Button();
+      this.lbl1 = new System.Windows.Forms.Label();
+      this.lbl2 = new System.Windows.Forms.Label();
+      this.lbl4 = new System.Windows.Forms.Label();
+      this.txtAdminPassword = new System.Windows.Forms.TextBox();
+      this.txtHost = new System.Windows.Forms.TextBox();
+      this.lbl3 = new System.Windows.Forms.Label();
+      this.groupBoxUserInfo = new System.Windows.Forms.GroupBox();
+      this.isUserLogin = new System.Windows.Forms.Button();
+      this.lbl8 = new System.Windows.Forms.Label();
+      this.txtResult = new System.Windows.Forms.TextBox();
+      this.lbl6 = new System.Windows.Forms.Label();
+      this.txtUserName = new System.Windows.Forms.TextBox();
+      this.lbl7 = new System.Windows.Forms.Label();
+      this.txtPassword = new System.Windows.Forms.TextBox();
+      this.isThereAUser = new System.Windows.Forms.Button();
+      this.statusStripVersion = new System.Windows.Forms.StatusStrip();
+      this.toolStripStatusLabelCopyright = new System.Windows.Forms.ToolStripStatusLabel();
+      this.toolStripStatusLabelVersion = new System.Windows.Forms.ToolStripStatusLabel();
+      this.groupBoxAdminInfo.SuspendLayout();
+      this.groupBoxUserInfo.SuspendLayout();
+      this.statusStripVersion.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // groupBoxAdminInfo
+      // 
+      this.groupBoxAdminInfo.Controls.Add(this.comboBoxConnectionType);
+      this.groupBoxAdminInfo.Controls.Add(this.lbl5);
+      this.groupBoxAdminInfo.Controls.Add(this.chckDisableCertCheck);
+      this.groupBoxAdminInfo.Controls.Add(this.comboBoxPort);
+      this.groupBoxAdminInfo.Controls.Add(this.txtAdminUserName);
+      this.groupBoxAdminInfo.Controls.Add(this.isAdminConnect);
+      this.groupBoxAdminInfo.Controls.Add(this.lbl1);
+      this.groupBoxAdminInfo.Controls.Add(this.lbl2);
+      this.groupBoxAdminInfo.Controls.Add(this.lbl4);
+      this.groupBoxAdminInfo.Controls.Add(this.txtAdminPassword);
+      this.groupBoxAdminInfo.Controls.Add(this.txtHost);
+      this.groupBoxAdminInfo.Controls.Add(this.lbl3);
+      this.groupBoxAdminInfo.Location = new System.Drawing.Point(12, 11);
+      this.groupBoxAdminInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.groupBoxAdminInfo.Name = "groupBoxAdminInfo";
+      this.groupBoxAdminInfo.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.groupBoxAdminInfo.Size = new System.Drawing.Size(270, 371);
+      this.groupBoxAdminInfo.TabIndex = 21;
+      this.groupBoxAdminInfo.TabStop = false;
+      this.groupBoxAdminInfo.Text = "LDAP Admin Bilgileri";
+      // 
+      // comboBoxConnectionType
+      // 
+      this.comboBoxConnectionType.FormattingEnabled = true;
+      this.comboBoxConnectionType.Items.AddRange(new object[] {
+            "ssl",
+            "starttls"});
+      this.comboBoxConnectionType.Location = new System.Drawing.Point(7, 265);
+      this.comboBoxConnectionType.Name = "comboBoxConnectionType";
+      this.comboBoxConnectionType.Size = new System.Drawing.Size(200, 24);
+      this.comboBoxConnectionType.TabIndex = 12;
+      // 
+      // lbl5
+      // 
+      this.lbl5.AutoSize = true;
+      this.lbl5.Location = new System.Drawing.Point(4, 244);
+      this.lbl5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl5.Name = "lbl5";
+      this.lbl5.Size = new System.Drawing.Size(82, 15);
+      this.lbl5.TabIndex = 13;
+      this.lbl5.Text = "Bağlantı Şekli";
+      // 
+      // chckDisableCertCheck
+      // 
+      this.chckDisableCertCheck.AutoSize = true;
+      this.chckDisableCertCheck.Checked = true;
+      this.chckDisableCertCheck.CheckState = System.Windows.Forms.CheckState.Checked;
+      this.chckDisableCertCheck.Location = new System.Drawing.Point(7, 296);
+      this.chckDisableCertCheck.Margin = new System.Windows.Forms.Padding(4);
+      this.chckDisableCertCheck.Name = "chckDisableCertCheck";
+      this.chckDisableCertCheck.Size = new System.Drawing.Size(163, 19);
+      this.chckDisableCertCheck.TabIndex = 20;
+      this.chckDisableCertCheck.Text = "Sertika Doğrulamayı Atla";
+      this.chckDisableCertCheck.UseVisualStyleBackColor = true;
+      // 
+      // comboBoxPort
+      // 
+      this.comboBoxPort.FormattingEnabled = true;
+      this.comboBoxPort.Items.AddRange(new object[] {
+            "389",
+            "636"});
+      this.comboBoxPort.Location = new System.Drawing.Point(7, 211);
+      this.comboBoxPort.Name = "comboBoxPort";
+      this.comboBoxPort.Size = new System.Drawing.Size(200, 24);
+      this.comboBoxPort.TabIndex = 10;
+      // 
+      // txtAdminUserName
+      // 
+      this.txtAdminUserName.Location = new System.Drawing.Point(7, 50);
+      this.txtAdminUserName.Margin = new System.Windows.Forms.Padding(4);
+      this.txtAdminUserName.Name = "txtAdminUserName";
+      this.txtAdminUserName.Size = new System.Drawing.Size(200, 20);
+      this.txtAdminUserName.TabIndex = 6;
+      // 
+      // isAdminConnect
+      // 
+      this.isAdminConnect.BackColor = System.Drawing.Color.Green;
+      this.isAdminConnect.ForeColor = System.Drawing.Color.White;
+      this.isAdminConnect.Location = new System.Drawing.Point(95, 324);
+      this.isAdminConnect.Margin = new System.Windows.Forms.Padding(4);
+      this.isAdminConnect.Name = "isAdminConnect";
+      this.isAdminConnect.Size = new System.Drawing.Size(168, 28);
+      this.isAdminConnect.TabIndex = 15;
+      this.isAdminConnect.Text = "LDAP Bağlan";
+      this.isAdminConnect.UseVisualStyleBackColor = false;
+      this.isAdminConnect.Click += new System.EventHandler(this.isAdminConnect_Click);
+      // 
+      // lbl1
+      // 
+      this.lbl1.AutoSize = true;
+      this.lbl1.Location = new System.Drawing.Point(3, 30);
+      this.lbl1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl1.Name = "lbl1";
+      this.lbl1.Size = new System.Drawing.Size(103, 15);
+      this.lbl1.TabIndex = 5;
+      this.lbl1.Text = "Admin Kullanı Adı";
+      // 
+      // lbl2
+      // 
+      this.lbl2.AutoSize = true;
+      this.lbl2.Location = new System.Drawing.Point(1, 83);
+      this.lbl2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl2.Name = "lbl2";
+      this.lbl2.Size = new System.Drawing.Size(70, 15);
+      this.lbl2.TabIndex = 7;
+      this.lbl2.Text = "Admin Şifre";
+      // 
+      // lbl4
+      // 
+      this.lbl4.AutoSize = true;
+      this.lbl4.Location = new System.Drawing.Point(4, 191);
+      this.lbl4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl4.Name = "lbl4";
+      this.lbl4.Size = new System.Drawing.Size(29, 15);
+      this.lbl4.TabIndex = 11;
+      this.lbl4.Text = "Port";
+      // 
+      // txtAdminPassword
+      // 
+      this.txtAdminPassword.Location = new System.Drawing.Point(7, 103);
+      this.txtAdminPassword.Margin = new System.Windows.Forms.Padding(4);
+      this.txtAdminPassword.Name = "txtAdminPassword";
+      this.txtAdminPassword.Size = new System.Drawing.Size(200, 20);
+      this.txtAdminPassword.TabIndex = 8;
+      // 
+      // txtHost
+      // 
+      this.txtHost.Location = new System.Drawing.Point(6, 156);
+      this.txtHost.Margin = new System.Windows.Forms.Padding(4);
+      this.txtHost.Name = "txtHost";
+      this.txtHost.Size = new System.Drawing.Size(200, 20);
+      this.txtHost.TabIndex = 10;
+      // 
+      // lbl3
+      // 
+      this.lbl3.AutoSize = true;
+      this.lbl3.Location = new System.Drawing.Point(2, 136);
+      this.lbl3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl3.Name = "lbl3";
+      this.lbl3.Size = new System.Drawing.Size(69, 15);
+      this.lbl3.TabIndex = 9;
+      this.lbl3.Text = "Host Adresi";
+      // 
+      // groupBoxUserInfo
+      // 
+      this.groupBoxUserInfo.Controls.Add(this.isUserLogin);
+      this.groupBoxUserInfo.Controls.Add(this.lbl8);
+      this.groupBoxUserInfo.Controls.Add(this.txtResult);
+      this.groupBoxUserInfo.Controls.Add(this.lbl6);
+      this.groupBoxUserInfo.Controls.Add(this.txtUserName);
+      this.groupBoxUserInfo.Controls.Add(this.lbl7);
+      this.groupBoxUserInfo.Controls.Add(this.txtPassword);
+      this.groupBoxUserInfo.Controls.Add(this.isThereAUser);
+      this.groupBoxUserInfo.Location = new System.Drawing.Point(301, 11);
+      this.groupBoxUserInfo.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.groupBoxUserInfo.Name = "groupBoxUserInfo";
+      this.groupBoxUserInfo.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+      this.groupBoxUserInfo.Size = new System.Drawing.Size(502, 371);
+      this.groupBoxUserInfo.TabIndex = 22;
+      this.groupBoxUserInfo.TabStop = false;
+      this.groupBoxUserInfo.Text = "Kullanıcı Bilgileri";
+      // 
+      // isUserLogin
+      // 
+      this.isUserLogin.BackColor = System.Drawing.Color.RoyalBlue;
+      this.isUserLogin.ForeColor = System.Drawing.Color.White;
+      this.isUserLogin.Location = new System.Drawing.Point(324, 100);
+      this.isUserLogin.Margin = new System.Windows.Forms.Padding(4);
+      this.isUserLogin.Name = "isUserLogin";
+      this.isUserLogin.Size = new System.Drawing.Size(168, 28);
+      this.isUserLogin.TabIndex = 17;
+      this.isUserLogin.Text = "Şifreyle Girişi Doğrula";
+      this.isUserLogin.UseVisualStyleBackColor = false;
+      this.isUserLogin.Click += new System.EventHandler(this.isUserLogin_Click);
+      // 
+      // lbl8
+      // 
+      this.lbl8.AutoSize = true;
+      this.lbl8.Location = new System.Drawing.Point(8, 138);
+      this.lbl8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl8.Name = "lbl8";
+      this.lbl8.Size = new System.Drawing.Size(50, 15);
+      this.lbl8.TabIndex = 16;
+      this.lbl8.Text = "SONUÇ";
+      // 
+      // txtResult
+      // 
+      this.txtResult.Location = new System.Drawing.Point(11, 158);
+      this.txtResult.Margin = new System.Windows.Forms.Padding(4);
+      this.txtResult.Multiline = true;
+      this.txtResult.Name = "txtResult";
+      this.txtResult.Size = new System.Drawing.Size(481, 140);
+      this.txtResult.TabIndex = 13;
+      // 
+      // lbl6
+      // 
+      this.lbl6.AutoSize = true;
+      this.lbl6.Location = new System.Drawing.Point(7, 30);
+      this.lbl6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl6.Name = "lbl6";
+      this.lbl6.Size = new System.Drawing.Size(54, 15);
+      this.lbl6.TabIndex = 0;
+      this.lbl6.Text = "Kullanıcı";
+      // 
+      // txtUserName
+      // 
+      this.txtUserName.Location = new System.Drawing.Point(11, 51);
+      this.txtUserName.Margin = new System.Windows.Forms.Padding(4);
+      this.txtUserName.Name = "txtUserName";
+      this.txtUserName.Size = new System.Drawing.Size(200, 20);
+      this.txtUserName.TabIndex = 1;
+      // 
+      // lbl7
+      // 
+      this.lbl7.AutoSize = true;
+      this.lbl7.Location = new System.Drawing.Point(7, 81);
+      this.lbl7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+      this.lbl7.Name = "lbl7";
+      this.lbl7.Size = new System.Drawing.Size(32, 15);
+      this.lbl7.TabIndex = 2;
+      this.lbl7.Text = "Şifre";
+      // 
+      // txtPassword
+      // 
+      this.txtPassword.Location = new System.Drawing.Point(11, 102);
+      this.txtPassword.Margin = new System.Windows.Forms.Padding(4);
+      this.txtPassword.Name = "txtPassword";
+      this.txtPassword.Size = new System.Drawing.Size(200, 20);
+      this.txtPassword.TabIndex = 3;
+      // 
+      // isThereAUser
+      // 
+      this.isThereAUser.BackColor = System.Drawing.Color.DarkOrange;
+      this.isThereAUser.ForeColor = System.Drawing.Color.White;
+      this.isThereAUser.Location = new System.Drawing.Point(324, 47);
+      this.isThereAUser.Margin = new System.Windows.Forms.Padding(4);
+      this.isThereAUser.Name = "isThereAUser";
+      this.isThereAUser.Size = new System.Drawing.Size(168, 28);
+      this.isThereAUser.TabIndex = 14;
+      this.isThereAUser.Text = "Kullanıcı Var Mı";
+      this.isThereAUser.UseVisualStyleBackColor = false;
+      this.isThereAUser.Click += new System.EventHandler(this.isThereAUser_Click);
+      // 
+      // statusStripVersion
+      // 
+      this.statusStripVersion.ImageScalingSize = new System.Drawing.Size(20, 20);
+      this.statusStripVersion.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabelCopyright,
+            this.toolStripStatusLabelVersion});
+      this.statusStripVersion.Location = new System.Drawing.Point(0, 391);
+      this.statusStripVersion.Name = "statusStripVersion";
+      this.statusStripVersion.Size = new System.Drawing.Size(818, 26);
+      this.statusStripVersion.TabIndex = 23;
+      this.statusStripVersion.Tag = "";
+      this.statusStripVersion.Text = "statusStrip1asda";
+      // 
+      // toolStripStatusLabelCopyright
+      // 
+      this.toolStripStatusLabelCopyright.Name = "toolStripStatusLabelCopyright";
+      this.toolStripStatusLabelCopyright.Size = new System.Drawing.Size(92, 20);
+      this.toolStripStatusLabelCopyright.Text = "yasinsunmaz";
+      // 
+      // toolStripStatusLabelVersion
+      // 
+      this.toolStripStatusLabelVersion.Name = "toolStripStatusLabelVersion";
+      this.toolStripStatusLabelVersion.Size = new System.Drawing.Size(35, 20);
+      this.toolStripStatusLabelVersion.Text = "v1.0";
+      // 
+      // DirectoryServicesLDAPForm
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(818, 417);
+      this.Controls.Add(this.statusStripVersion);
+      this.Controls.Add(this.groupBoxUserInfo);
+      this.Controls.Add(this.groupBoxAdminInfo);
+      this.Name = "DirectoryServicesLDAPForm";
+      this.Text = "DirectoryServices LDAP Kimlik Doğrulama";
+      this.groupBoxAdminInfo.ResumeLayout(false);
+      this.groupBoxAdminInfo.PerformLayout();
+      this.groupBoxUserInfo.ResumeLayout(false);
+      this.groupBoxUserInfo.PerformLayout();
+      this.statusStripVersion.ResumeLayout(false);
+      this.statusStripVersion.PerformLayout();
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxAdminInfo;
+        private System.Windows.Forms.TextBox txtAdminUserName;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl4;
+        private System.Windows.Forms.TextBox txtAdminPassword;
+        private System.Windows.Forms.TextBox txtHost;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.GroupBox groupBoxUserInfo;
+        private System.Windows.Forms.CheckBox chckDisableCertCheck;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lbl6;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.Label lbl7;
+        private System.Windows.Forms.Button isAdminConnect;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button isThereAUser;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.StatusStrip statusStripVersion;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelVersion;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabelCopyright;
+        private System.Windows.Forms.ComboBox comboBoxConnectionType;
+        private System.Windows.Forms.Label lbl5;
+        private System.Windows.Forms.Label lbl8;
+        private System.Windows.Forms.Button isUserLogin;
+  }
+}
+
---
+++ DirectoryServicesLDAP/DirectoryServicesLDAPForm.cs
@@ -0,0 +1,297 @@
+﻿using System;
+using System.DirectoryServices.Protocols;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DirectoryServicesLDAP
+{
+  public partial class DirectoryServicesLDAPForm : Form
+  {
+    public DirectoryServicesLDAPForm()
+    {
+      InitializeComponent();
+    }
+
+    private System.DirectoryServices.Protocols.LdapConnection _ldapConnectionAdmin;
+    private SearchResponse _ldapSearchResponse;
+    private string userName;
+    private string userDNInfo;
+
+    private void isAdminConnect_Click(object sender, EventArgs e)
+    {
+      bool isAdminLdapConnect = isAdminConnectDAP();
+      if (isAdminLdapConnect)
+      {
+        MessageBox.Show("LDAP Bağlantısı Başarılı!");
+      }
+      else
+      {
+        MessageBox.Show("LDAP Bağlantısı Başarısız!");
+      }
+    }
+
+    private bool isAdminConnectDAP()
+    {
+      bool status = false;
+      string host = txtHost.Text;
+      string portValue = comboBoxPort.Text;
+      string adminUser = txtAdminUserName.Text;
+      string adminPass = txtAdminPassword.Text;
+      string ldapSecurityMethod = comboBoxConnectionType.Text;
+      bool isDisableCertCheck = chckDisableCertCheck.Checked;
+
+      try
+      {
+        if (isAdminValidation(host, portValue, adminUser, adminPass, ldapSecurityMethod))
+        {
+          int port = int.Parse(comboBoxPort.Text);
+          _ldapConnectionAdmin = new System.DirectoryServices.Protocols.LdapConnection(host + ":" + port);
+
+          NetworkCredential authAdmin = new System.Net.NetworkCredential(adminUser, adminPass);
+          _ldapConnectionAdmin.Credential = authAdmin;
+          _ldapConnectionAdmin.AuthType = AuthType.Basic;
+          _ldapConnectionAdmin.SessionOptions.ProtocolVersion = 3;
+          if (port == 636)
+          {
+            ldapProtocolsSecurityMethod(_ldapConnectionAdmin, ldapSecurityMethod, isDisableCertCheck);
+          }
+          _ldapConnectionAdmin.Bind();
+          status = true;
+        }
+        else
+        {
+          MessageBox.Show("Tüm bilgiler girilmelidir!");
+        }
+        return status;
+      }
+      catch (Exception ex)
+      {
+        _ldapConnectionAdmin.Dispose();
+        txtResult.Text = "Admin LDAP bağlantısı gerçekleşmedi! Mesaj: " + ex.Message;
+        return status;
+      }
+    }
+
+    private bool isAdminValidation(string host, string port, string adminUser, string adminPass, string ldapSecurityMethod)
+    {
+      if (host == "" || port == "" || adminUser == "" || adminPass == "" || ldapSecurityMethod == "")
+      {
+        return false;
+      }
+      return true;
+    }
+
+    private void isThereAUser_Click(object sender, EventArgs e)
+    {
+      bool isThereUser = isThereAUserValid();
+      if (isThereUser)
+      {
+        MessageBox.Show("Kullanıcı bulundu!");
+      }
+      else
+      {
+        MessageBox.Show("Kullanıcı bulunamadı!");
+      }
+    }
+
+    private bool isThereAUserValid()
+    {
+      bool status = false;
+      try
+      {
+        if (!string.IsNullOrEmpty(txtUserName.Text))
+        {
+          userName = txtUserName.Text;
+          //LDAP aranacak DN bilgisi
+          string dn = "dc=etu";
+
+          //LDAP injection saldırılarını önlemek için LDAP arama filtresinden çıkarma
+          userName = EscapeLdapSearchFilter(userName);
+
+          System.DirectoryServices.Protocols.SearchRequest ldapSearchRequest;
+          ldapSearchRequest = new SearchRequest(dn, string.Format("(&(objectClass=*)(status=active)(|(identity=" + userName + ")(email=" + userName + ")))"), SearchScope.Subtree, null);
+          if (_ldapConnectionAdmin != null)
+          {
+            _ldapSearchResponse = (SearchResponse)_ldapConnectionAdmin.SendRequest(ldapSearchRequest);
+            if (_ldapSearchResponse.Entries.Count > 0)
+            {
+              userDNInfo = _ldapSearchResponse.Entries[0].DistinguishedName;
+              //LDAP kullanıcısının detaylı bilgilerini alma - Kullanıcıya ait email bilgisini çekme
+              //var attrValue = rsp.Entries[0].Attributes["email"][0].ToString();
+              status = true;
+            }
+          }
+          else
+          {
+            MessageBox.Show("Öncelikle LDAP Bağlantısı Yapmalısınız!");
+          }
+        }
+        else
+        {
+          MessageBox.Show("Kullanıcı boş geçilemez!");
+        }
+
+        return status;
+      }
+      catch (Exception ex)
+      {
+        _ldapConnectionAdmin.Dispose();
+        txtResult.Text = "Kullanıcı bulunamadı! Mesaj: " + ex.Message;
+        return status;
+      }
+    }
+
+    private void isUserLogin_Click(object sender, EventArgs e)
+    {
+      bool isUserLogin = isUserLoginAuth();
+      if (isUserLogin)
+      {
+        MessageBox.Show("Kullanıcı giriş doğrulandı!");
+      }
+      else
+      {
+        MessageBox.Show("Kullanıcı girişi başarısız!");
+      }
+    }
+
+    private bool isUserLoginAuth()
+    {
+      bool status = false;
+
+      //Mevcut kullanıcı dn  bilgisi varsa direk giriş doğrulama yapılsın
+      if (userDNInfo != null)
+      {
+        return isUserLoginAuthentication();
+      }
+      else
+      {
+        //Kullanıcıya ait dn bilgisi yoksa önce kullanıcı var mı bakılıp dn bilgisinin dolması sağlanır
+        if (isThereAUserValid())
+        {
+          //Kullanıcı bulunduktan sonra dn bilgisi doldurulduğu için giriş doğrulama yapılsın
+          return isUserLoginAuthentication();
+        }
+      }
+      return status;
+    }
+
+    private bool isUserLoginAuthentication()
+    {
+      bool status = false;
+      string host = txtHost.Text;
+      string portValue = comboBoxPort.Text;
+      string ldapSecurityMethod = comboBoxConnectionType.Text;
+      bool isDisableCertCheck = chckDisableCertCheck.Checked;
+      string userPassword = txtPassword.Text;
+      userName = txtUserName.Text;
+      if (isUserValidation(host, portValue, userName, userPassword, ldapSecurityMethod))
+      {
+        int port = int.Parse(comboBoxPort.Text);
+        System.DirectoryServices.Protocols.LdapConnection connUserLogin = new System.DirectoryServices.Protocols.LdapConnection(host + ":" + port);
+        try
+        {
+          NetworkCredential auth2 = new System.Net.NetworkCredential(userDNInfo, userPassword);
+          connUserLogin.Credential = auth2;
+          connUserLogin.AuthType = AuthType.Basic;
+
+          connUserLogin.SessionOptions.ProtocolVersion = 3;
+
+          if (port == 636)
+          {
+            ldapProtocolsSecurityMethod(connUserLogin, ldapSecurityMethod, isDisableCertCheck);
+          }
+
+          connUserLogin.Bind();
+          status = true;
+          connUserLogin.Dispose();
+        }
+        catch (Exception ex)
+        {
+          txtResult.Text = "Kullanıcı giriş doğrulaması başarısız! Mesaj: " + ex.Message;
+          connUserLogin.Dispose();
+        }
+      }
+      else
+      {
+        MessageBox.Show("Host, port, bağlantı şekli, kullanıcı ve şifre alanları girilmelidir!");
+      }
+      return status;
+    }
+
+    private bool isUserValidation(string host, string port, string userName, string userPassword, string ldapSecurityMethod)
+    {
+      if (host == "" || port == "" || userName == "" || userPassword == "" || ldapSecurityMethod == "")
+      {
+        return false;
+      }
+      return true;
+    }
+
+    private static void ldapProtocolsSecurityMethod(System.DirectoryServices.Protocols.LdapConnection _ldapProtocolsConnection, string ldapSecurityMethod, bool isDisableCertCheck)
+    {
+      if (ldapSecurityMethod == "ssl")
+      {
+        if (isDisableCertCheck)
+        {
+          _ldapProtocolsConnection.SessionOptions.VerifyServerCertificate += delegate { return true; };
+        }
+        _ldapProtocolsConnection.SessionOptions.SecureSocketLayer = true;
+      }
+      else if (ldapSecurityMethod == "starttls")
+      {
+        if (isDisableCertCheck)
+        {
+          _ldapProtocolsConnection.SessionOptions.VerifyServerCertificate += delegate { return true; };
+        }
+        _ldapProtocolsConnection.SessionOptions.SecureSocketLayer = false;
+        _ldapProtocolsConnection.SessionOptions.StartTransportLayerSecurity(null);
+      }
+    }
+
+    /// <summary>
+    /// Escapes the LDAP search filter to prevent LDAP injection attacks.
+    /// </summary>
+    /// <param name="searchFilter">The search filter.</param>
+    /// <returns>The escaped search filter.</returns>
+    private static string EscapeLdapSearchFilter(string searchFilter)
+    {
+      var escape = new StringBuilder();
+      foreach (var current in searchFilter)
+      {
+        switch (current)
+        {
+          case '\\':
+            escape.Append(@"\5c");
+            break;
+
+          case '*':
+            escape.Append(@"\2a");
+            break;
+
+          case '(':
+            escape.Append(@"\28");
+            break;
+
+          case ')':
+            escape.Append(@"\29");
+            break;
+
+          case '\u0000':
+            escape.Append(@"\00");
+            break;
+
+          case '/':
+            escape.Append(@"\2f");
+            break;
+
+          default:
+            escape.Append(current);
+            break;
+        }
+      }
+
+      return escape.ToString();
+    }
+  }
+}
